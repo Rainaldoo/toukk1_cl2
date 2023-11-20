223db8f696a59f51760ccbfc44f52bf014b64a53
Rainaldo <rrainaldo067@gmail.com>
2023-11-20 11:56:29 +0700

    first commit

---
+++ Lat ukk C#/Program.cs
@@ -0,0 +1,44 @@
+﻿String nama;
+int harian,uts,uas;
+double rata;
+
+Console.Write("Nama             :");
+nama = Console.ReadLine();
+
+Console.Write("Nilai Harian     :");
+harian = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Nilai UTS        :");
+uts = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Nilai UAS        :");
+uas = Convert.ToInt32(Console.ReadLine());
+
+rata = (harian+uts+uas)/3;
+Console.WriteLine("=====================");
+Console.WriteLine("Nama         : "+nama);
+Console.WriteLine("Nilai Harian : "+harian);
+Console.WriteLine("Nilai UTS    : "+uts);
+Console.WriteLine("Nilai UAS    : "+uas);
+Console.WriteLine("Rata         : "+rata);
+
+if (rata>=80)
+{
+    Console.WriteLine("Predikat     : A");
+}
+else if (rata>=70)
+{
+    Console.WriteLine("Predikat     : B");
+}
+else if (rata>=60)
+{
+    Console.WriteLine("Predikat     : C");
+}
+else if (rata>=50)
+{
+    Console.WriteLine("Predikat     : D");
+}
+else
+{
+    Console.WriteLine("Predikat     : E");
+}
